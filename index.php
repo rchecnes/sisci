0cd4fbf276514147d7c2686286c8797821c5cac9
rchecnes <rchecnes@acuario.com.pe>
2017-07-27 17:51:54 -0500

    change

--- index.php
+++ index.php
@@ -1,25 +1,4 @@
 <?php
-/*//Configuración global
-require_once 'Config/global.php';
- 
-//Base para los controladores
-require_once 'Core/ControllerBase.php';
- 
-//Funciones para el controlador frontal
-require_once 'Core/ControllerFrontal.func.php';
- 
-//Cargamos controladores y acciones
-if(isset($_GET["controller"])){
-    $controllerObj=cargarControlador($_GET["controller"]);
-    lanzarAccion($controllerObj);
-}else{
-    $controllerObj=cargarControlador(CONTROLADOR_DEFECTO);
-    lanzarAccion($controllerObj);
-}*/
-
-
-
-//SEGUNDA FORMA
 require_once 'Config/global.php';
 
 $url = isset($_GET['url'])?$_GET['url']:CONTROLADOR_DEFECTO."/".ACCION_DEFECTO;
